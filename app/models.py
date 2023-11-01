60a8a1209bef75d91940157ec6dfbec80278a279
Harsh Bosmaya <harshbosmaya@gmail.com>
2023-11-01 15:55:41 -0700

    Mark 2 build

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class contacts(models.Model):
-    name=models.CharField(max_length=25,blank=False,null=False)
-    email=models.EmailField()
+    name=models.CharField(max_length=25,blank=False,null=False, unique=True)
+    email=models.EmailField(unique=True)
     time=models.TimeField(auto_now_add=True)
     
     def __str__(self) :
--- app/views.py
+++ app/views.py
@@ -4,23 +4,44 @@ from django.contrib import messages
 from datetime import datetime
 from django.db import models
 from django.utils import timezone
+from django.db.models import Max
+from django.db.models import F
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
     data=contacts.objects.all()
     print(data)
     context={"data":data}
+    # data = {'name':data[0].name,'email':data[0].email,'time':data[0].time}
+    # context={"data":}
     return render(request,"index.html",context)
 
 def insertData(request):
-    if request.method=="POST":
+    count= contacts.objects.all().count()
+    if request.method=="POST" and count == 0:
+        initial_id = 1
         name=request.POST.get('name')
         email=request.POST.get('email')
         time = models.DateTimeField(auto_now_add=True)
         print(contacts.objects.all())
-        query=contacts(name=name,email=email)
+        query=contacts(id=initial_id,name=name,email=email)
         query.save()
         messages.info(request,"Data Inserted Successfully")
+        return redirect("/")
+    if request.method=="POST" and count > 0:
+        max_id = contacts.objects.aggregate(Max('id'))['id__max'] or 0
+        next_id = max_id + 1
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        time = models.DateTimeField(auto_now_add=True)
+        if contacts.objects.filter(Q(name=name) | Q(email=email)).exists():
+            messages.info(request, "Email and/or Name already exists in records.")
+        else:
+            print(contacts.objects.all())
+            query=contacts(id=next_id,name=name,email=email)
+            query.save()
+            messages.info(request,"Data Inserted Successfully")
         return redirect("/")
 
     return render(request,"index.html")
@@ -33,12 +54,14 @@ def updateData(request,id):
         # time = models.DateTimeField(auto_now_add=True)
         time = models.DateTimeField(auto_now_add=True)
         print(contacts.objects.all())
-
-        edit=contacts.objects.get(id=id)
-        edit.name=name
-        edit.email=email
-        edit.save()
-        messages.warning(request,"Data Updated Successfully")
+        if contacts.objects.filter(Q(name=name) | Q(email=email)).exists():
+            messages.info(request, "Email and/or Name already exists in records.")
+        else:
+            edit=contacts.objects.get(id=id)
+            edit.name=name
+            edit.email=email
+            edit.save()
+            messages.warning(request,"Data Updated Successfully")
         return redirect("/")
 
     d=contacts.objects.get(id=id) 
@@ -49,6 +72,7 @@ def deleteData(request,id):
     d=contacts.objects.get(id=id) 
     d.delete()
     messages.error(request,"Data deleted Successfully")
+    contacts.objects.filter(id__gt=id).update(id=F('id') - 1)
     return redirect("/")
 
 def about(request):
