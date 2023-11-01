45a3a0cfe45e464348d7194da9cbdcedf607c696
Harsh Bosmaya <harshbosmaya@gmail.com>
2023-10-31 23:51:06 -0700

    Mark1 code ready

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class contacts(models.Model):
+    name=models.CharField(max_length=25,blank=False,null=False)
+    email=models.EmailField()
+    time=models.TimeField(auto_now_add=True)
+    
+    def __str__(self) :
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import contacts
+from django.contrib import messages
+from datetime import datetime
+from django.db import models
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
+    data=contacts.objects.all()
+    print(data)
+    context={"data":data}
+    return render(request,"index.html",context)
+
+def insertData(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        time = models.DateTimeField(auto_now_add=True)
+        print(contacts.objects.all())
+        query=contacts(name=name,email=email)
+        query.save()
+        messages.info(request,"Data Inserted Successfully")
+        return redirect("/")
+
     return render(request,"index.html")
+
+
+def updateData(request,id):
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        # time = models.DateTimeField(auto_now_add=True)
+        time = models.DateTimeField(auto_now_add=True)
+        print(contacts.objects.all())
+
+        edit=contacts.objects.get(id=id)
+        edit.name=name
+        edit.email=email
+        edit.save()
+        messages.warning(request,"Data Updated Successfully")
+        return redirect("/")
+
+    d=contacts.objects.get(id=id) 
+    context={"d":d}
+    return render(request,"edit.html",context)
+
+def deleteData(request,id):
+    d=contacts.objects.get(id=id) 
+    d.delete()
+    messages.error(request,"Data deleted Successfully")
+    return redirect("/")
 
 def about(request):
     return render(request,"about.html")
